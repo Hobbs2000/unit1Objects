79dba395dfa99a94d8d6429c6dbd79efaf20c038
Hobbs2000 <cjfield@NN121-HA-18.HS.sd203.org>
2015-08-21 10:21:58 -0500

    Used For loops on the turtle

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -4,12 +4,26 @@ public class TurtleWorld
 {
     public static void main(String args[])
     {
+        int go = 10;
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
         turtle.setPenColor(Color.RED);
         turtle.penDown();
         turtle.forward(50);
-        turtle.turn(100);
-        turtle.back(50);
+        for (;go > 0;){
+            go += 1;
+            turtle.turn(90);
+            turtle.setPenColor(Color.RED);
+            turtle.forward(go);
+            turtle.turn(90);
+            turtle.setPenColor(Color.BLUE);
+            go += 1;
+            turtle.forward(go);
+            turtle.turn(90);
+            turtle.setPenColor(Color.GREEN);
+            go += 1;
+            turtle.forward(go);
+            
+        }
     }
 }
