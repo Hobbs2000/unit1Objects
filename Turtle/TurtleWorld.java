d9f3ce2c19683289e9c7002dea78add02daaf495
Hobbs2000 <cjfield@NN121-HA-18.HS.sd203.org>
2015-08-24 10:28:42 -0500

    Made a new class for printing onto the console

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -4,25 +4,19 @@ public class TurtleWorld
 {
     public static void main(String args[])
     {
-        int go = 10;
+        int go = 0;
+        int turn = 0;
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
         turtle.setPenColor(Color.RED);
         turtle.penDown();
         turtle.forward(50);
-        for (;go > 0;){
+        for (;go > -1;){
             go += 1;
-            turtle.turn(90);
-            turtle.setPenColor(Color.RED);
+            turn += 1;
             turtle.forward(go);
-            turtle.turn(90);
-            turtle.setPenColor(Color.BLUE);
-            go += 1;
-            turtle.forward(go);
-            turtle.turn(90);
-            turtle.setPenColor(Color.GREEN);
-            go += 1;
-            turtle.forward(go);
+            turtle.turn(turn);
+
             
         }
     }
---
+++ Unit1 Practice Classes/HelloPrinter.java
@@ -0,0 +1,13 @@
+public class HelloPrinter
+{
+    public static void main( String[] args )
+    {
+        // Display greeting in the console window
+        System.out.println( "Hello, World!" );
+        for (;2<3;) 
+        {
+            System.out.println( "101010101010101010101010101010101010101010101010101010101" ); 
+            System.out.println( "010101010101010101010101010101010101010101010101010101010" );
+        }
+    }
+}
