1d5448176020c6043f5e2b15661bffa0f844a1a2
Hobbs2000 <cjfield@NN121-HA-18.HS.sd203.org>
2015-09-01 10:31:06 -0500

    Worked with Jframe and Color

--- Unit2 practice classes/BrighterDemo.java
+++ Unit2 practice classes/BrighterDemo.java
@@ -4,12 +4,33 @@ public class BrighterDemo
 {
     public static void main(String[] args) 
     {
-        Color c1 = new Color(50,100,150);
+        Color c1 = new Color(0,0,255);
+        Color c2 = new Color(255,0,0);
+        Color c3 = new Color(0,255,0);
         
         JFrame frame = new JFrame();
-        frame.setSize(1920, 1080);
+        frame.setSize(300, 300);
         frame.getContentPane().setBackground(c1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        int go = 0;
+        
+        for (;go > -1;)
+        {
+            go += 1;
+            frame.getContentPane().setBackground(c1);
+            frame.getContentPane().setBackground(c2);
+            frame.getContentPane().setBackground(c3);
+            if ( go % 2 == 0)
+            {
+                //Means its even
+                frame.setSize(300,300);
+            }
+            else
+            {
+                //Means its even
+                frame.setSize(200,200);
+            }
+        }
     }
 }
